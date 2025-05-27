c8f721bb8c50876c5c0b8182389b37cc58b6bae2
Luis-E-Ortega <lorte030@gmail.com>
2025-05-27 18:34:29 -0400

    Added SetUser function and helper functions

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
 type Config struct {
@@ -11,22 +12,68 @@ type Config struct {
 }
 
 func Read() (Config, error) {
-	con := Config{}
-	homeDirectory, err := os.UserHomeDir()
-	if err != nil {
-		return con, err
-	}
-	gatorDir := homeDirectory + "/.gatorconfig.json"
+	// Initialize instance of Config struct
+	config := Config{}
 
+	// Run helper function to get Config file path
+	gatorDir, err := getConfigFilePath()
+
+	// Read data from file given the full path
 	data, err := os.ReadFile(gatorDir)
 	if err != nil {
-		return con, err
+		return config, err
 	}
 
-	err = json.Unmarshal(data, &con)
+	// Unmarshal into config struct instance we created
+	err = json.Unmarshal(data, &config)
 	if err != nil {
-		return con, err
+		return config, err
 	}
 
-	return con, nil
+	return config, nil
+}
+
+func getConfigFilePath() (string, error) {
+	const configFileName = "/.gatorconfig.json"
+	homeDirectory, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	// Use filepath to join paths for full portable path, usable across different systems
+	fullPath := filepath.Join(homeDirectory, configFileName)
+
+	return fullPath, nil
+}
+
+func write(cfg Config) error {
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filePath, data, 0600)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Config) SetUser(username string) error {
+	c.CurrentUserName = username
+	err := write(*c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+
 }
