bea57b6ab7b2db6067150af563903ae4518d37d9
Luis-E-Ortega <lorte030@gmail.com>
2025-05-28 17:09:38 -0400

    Completed config package and implemented main

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/Luis-E-Ortega/gatorcli/internal/config"
+)
+
 func main() {
+	data, err := config.Read()
+	if err != nil {
+		fmt.Println("Error reading file")
+		return
+	}
+
+	err = data.SetUser("Luis")
+	if err != nil {
+		fmt.Println("Error setting user")
+		return
+	}
+
+	updatedData, err := config.Read()
+	if err != nil {
+		fmt.Println("Error reading file")
+		return
+	}
+
+	printableData, err := json.Marshal(updatedData)
+	if err != nil {
+		fmt.Println("Error marshalling file")
+		return
+	}
+
+	stringVersion := string(printableData)
+
+	fmt.Println(stringVersion)
 
 }
